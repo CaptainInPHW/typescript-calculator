87ab579515e1804d506c82955b9f5a569afbba0a
jiangtai.dai <jiangtao.dai@starwin.com>
2019-04-25 17:52:16 +0800

    update

---
+++ calculator.js
@@ -0,0 +1,123 @@
+var Calculator = /** @class */ (function () {
+    function Calculator(rootElement) {
+        this.rootElement = rootElement;
+        this.KEYS = [
+            ['AC', '÷'],
+            [7, 8, 9, '×'],
+            [4, 5, 6, '﹣'],
+            [1, 2, 3, '﹢'],
+            [0, '.', '=']
+        ];
+        this.NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        this.OPERATORS = ['÷', '×', '﹢', '﹣'];
+        this.DECIMAL_POINT = '.';
+        this.EXECUTE_FLAG = '=';
+        this.CLEAR_FLAG = 'AC';
+        this.x = '';
+        this.y = '';
+        this.operator = '';
+        this.result = '';
+    }
+    Calculator.prototype.create = function () {
+        this.createCalculatorContainer();
+        this.createDecorator();
+        this.createResultDisplayContainer();
+        this.createResultDisplayElement();
+        this.createButtons();
+        this.addEventListener();
+    };
+    Calculator.prototype.createCalculatorContainer = function () {
+        this.calculator = this.createElement('div');
+        this.addClass(this.calculator, 'calculator');
+        this.rootElement.prepend(this.calculator);
+    };
+    Calculator.prototype.createDecorator = function () {
+        var dot = this.createElement('div');
+        var container = this.createElement('div');
+        this.addClass(dot, 'dot');
+        this.addClass(container, 'decorator');
+        container.appendChild(dot);
+        this.calculator.appendChild(container);
+    };
+    Calculator.prototype.createResultDisplayContainer = function () {
+        this.displayContainer = this.createElement('div');
+        this.addClass(this.displayContainer, 'display');
+        this.calculator.appendChild(this.displayContainer);
+    };
+    Calculator.prototype.createResultDisplayElement = function () {
+        this.resultElement = this.createElement('div');
+        this.addClass(this.resultElement, 'result');
+        this.resultElement.textContent = '0';
+        this.displayContainer.appendChild(this.resultElement);
+    };
+    Calculator.prototype.createButtons = function () {
+        var _this = this;
+        this.KEYS.forEach(function (rowKeys) {
+            var row = _this.createElement('div');
+            _this.addClass(row, 'row');
+            _this.calculator.appendChild(row);
+            rowKeys.forEach(function (key) {
+                var button = _this.createElement('div');
+                _this.addClass(button, 'button');
+                button.textContent = "" + key;
+                row.appendChild(button);
+            });
+        });
+    };
+    Calculator.prototype.addEventListener = function () {
+        var _this = this;
+        this.calculator.addEventListener('click', function (event) {
+            var target = event.target;
+            var className = target.className;
+            if (className === 'button') {
+                var text = target.textContent;
+                if (_this.NUMBERS.indexOf(Number(text)) > -1) {
+                    if (!_this.operator) {
+                        _this.x += text;
+                        _this.updateResult(_this.x);
+                    }
+                    else {
+                        _this.y += text;
+                        _this.updateResult(_this.y);
+                    }
+                }
+                else if (_this.OPERATORS.indexOf(text) > -1) {
+                    _this.operator = text;
+                }
+                else if (_this.EXECUTE_FLAG === text) {
+                    _this.result = _this.excuteAlgorithm();
+                    _this.updateResult(_this.result);
+                }
+                else if (_this.CLEAR_FLAG === text) {
+                    _this.x = '';
+                    _this.y = '';
+                    _this.operator = '';
+                    _this.result = '';
+                    _this.updateResult('0');
+                }
+            }
+        });
+    };
+    Calculator.prototype.updateResult = function (result) {
+        this.resultElement.textContent = result;
+    };
+    Calculator.prototype.excuteAlgorithm = function () {
+        switch (this.operator) {
+            case '﹢':
+                return "" + (Number(this.x) + Number(this.y));
+            case '-':
+                return "" + (Number(this.x) - Number(this.y));
+            case '×':
+                return "" + Number(this.x) * Number(this.y);
+            case '÷':
+                return "" + Number(this.x) / Number(this.y);
+        }
+    };
+    Calculator.prototype.createElement = function (tag) {
+        return document.createElement(tag);
+    };
+    Calculator.prototype.addClass = function (target, className) {
+        target.classList.add(className);
+    };
+    return Calculator;
+}());
--- calculator.ts
+++ calculator.ts
@@ -1,3 +1,118 @@
-class Calcultor {
-  constructor(public root: HTMLElement) {}
+class Calculator {
+  constructor(private rootElement: HTMLElement) {}
+  private readonly KEYS: Array<Array<number | string>> = [
+    ['AC', '÷'],
+    [7, 8, 9, '×'],
+    [4, 5, 6, '﹣'],
+    [1, 2, 3, '﹢'],
+    [0, '.', '=']
+  ];
+  private readonly NUMBERS: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  private readonly OPERATORS: Array<string> = ['÷', '×', '﹢', '﹣'];
+  private readonly DECIMAL_POINT: string = '.';
+  private readonly EXECUTE_FLAG: string = '=';
+  private readonly CLEAR_FLAG: string = 'AC';
+  private calculator: HTMLElement;
+  private displayContainer: HTMLElement;
+  private resultElement: HTMLElement;
+  private x: string = '';
+  private y: string = '';
+  private operator: string = '';
+  private result: string = '';
+
+  public create(): void {
+    this.createCalculatorContainer();
+    this.createDecorator();
+    this.createResultDisplayContainer();
+    this.createResultDisplayElement();
+    this.createButtons();
+    this.addEventListener();
+  }
+  private createCalculatorContainer(): void {
+    this.calculator = this.createElement('div');
+    this.addClass(this.calculator, 'calculator');
+    this.rootElement.prepend(this.calculator);
+  }
+  private createDecorator(): void {
+    const dot: HTMLElement = this.createElement('div');
+    const container: HTMLElement = this.createElement('div');
+    this.addClass(dot, 'dot');
+    this.addClass(container, 'decorator');
+    container.appendChild(dot);
+    this.calculator.appendChild(container);
+  }
+  private createResultDisplayContainer(): void {
+    this.displayContainer = this.createElement('div');
+    this.addClass(this.displayContainer, 'display');
+    this.calculator.appendChild(this.displayContainer);
+  }
+  private createResultDisplayElement(): void {
+    this.resultElement = this.createElement('div');
+    this.addClass(this.resultElement, 'result');
+    this.resultElement.textContent = '0';
+    this.displayContainer.appendChild(this.resultElement);
+  }
+  private createButtons(): void {
+    this.KEYS.forEach((rowKeys: Array<string | number>) => {
+      const row: HTMLElement = this.createElement('div');
+      this.addClass(row, 'row');
+      this.calculator.appendChild(row);
+      rowKeys.forEach((key: string | number) => {
+        const button: HTMLElement = this.createElement('div');
+        this.addClass(button, 'button');
+        button.textContent = `${key}`;
+        row.appendChild(button);
+      });
+    });
+  }
+  private addEventListener(): void {
+    this.calculator.addEventListener('click', (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      const { className } = target;
+      if (className === 'button') {
+        const text = target.textContent;
+        if (this.NUMBERS.indexOf(Number(text)) > -1) {
+          if (!this.operator) {
+            this.x += text;
+            this.updateResult(this.x);
+          } else {
+            this.y += text;
+            this.updateResult(this.y);
+          }
+        } else if (this.OPERATORS.indexOf(text) > -1) {
+          this.operator = text;
+        } else if (this.EXECUTE_FLAG === text) {
+          this.result = this.excuteAlgorithm();
+          this.updateResult(this.result);
+        } else if (this.CLEAR_FLAG === text) {
+          this.x = '';
+          this.y = '';
+          this.operator = '';
+          this.result = '';
+          this.updateResult('0');
+        }
+      }
+    });
+  }
+  private updateResult(result: string): void {
+    this.resultElement.textContent = result;
+  }
+  private excuteAlgorithm(): string {
+    switch (this.operator) {
+      case '﹢':
+        return `${Number(this.x) + Number(this.y)}`;
+      case '-':
+        return `${Number(this.x) - Number(this.y)}`;
+      case '×':
+        return `${Number(this.x) * Number(this.y)}`;
+      case '÷':
+        return `${Number(this.x) / Number(this.y)}`;
+    }
+  }
+  private createElement(tag: string): HTMLElement {
+    return document.createElement(tag);
+  }
+  private addClass(target: HTMLElement, className: string): void {
+    target.classList.add(className);
+  }
 }
